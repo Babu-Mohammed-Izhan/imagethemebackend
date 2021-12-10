1243b5c09f5a5f6417fd5b19b131108ad6479694
Izhan <babuizhan1234@gmail.com>
2021-12-10 20:07:53 +0530

    Added env support and given mongodb connection

--- index.ts
+++ index.ts
@@ -1,10 +1,22 @@
+import * as dotenv from "dotenv";
+dotenv.config({ path: __dirname + "/.env" });
 import express from "express";
 import cors from "cors";
 import noteRouter from "./controller/note";
+import mongoose from "mongoose";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+mongoose
+  .connect(`${process.env.MONGO_URL}`)
+  .then(() => {
+    console.log("connected to MongoDB database");
+  })
+  .catch((err) => {
+    console.log("error connecting to MongoDB: ", err.message);
+  });
 
 const PORT = 3001;
 
--- utils.ts
+++ utils.ts
@@ -5,10 +5,6 @@ const isString = (text: unknown): text is string => {
   return typeof text === "string";
 };
 
-const isDate = (date: string): boolean => {
-  return Boolean(Date.parse(date));
-};
-
 const parseNumber = (num: unknown): number => {
   if (!num || !isNumber(num)) {
     throw new Error("Incorrect or missing number input");
@@ -25,11 +21,4 @@ const parseString = (comment: unknown): string => {
   return comment;
 };
 
-const parseDate = (date: unknown): string => {
-  if (!date || !isString(date) || !isDate(date)) {
-    throw new Error("Incorrect or missing date: " + date);
-  }
-  return date;
-};
-
-export { parseDate, parseString, parseNumber };
+export { parseString, parseNumber };
