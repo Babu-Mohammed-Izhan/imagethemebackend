2fdb599677cb7646914a7f433268a584941d794e
Izhan <babuizhan1234@gmail.com>
2022-01-30 16:38:10 +0530

    Added color theif extraction

--- controller/upload.ts
+++ controller/upload.ts
@@ -1,8 +1,9 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/no-unsafe-argument */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
-import axios from 'axios';
 import express, { Request, Response } from 'express';
-import { parseString } from '../utils';
+import { getPaletteFromURL } from 'color-thief-node';
+import { parseString, rgbToHex } from '../utils';
 import imageService from '../service/uploadservice';
 
 const router = express.Router();
@@ -20,58 +21,25 @@ router.get('/', (_req: Request, res: Response) => {
 
 router.post('/', (req: Request, res: Response) => {
   const imgurl: string = parseString(req.body.url);
-  const apiKey = 'acc_3e5d16f9e4f3006';
-  const apiSecret = '498e6d9f842f3eb8669809adffbff38a';
-
-  const url =
-    'https://api.imagga.com/v2/colors?image_url=' + encodeURIComponent(imgurl);
 
   void (async () => {
-    try {
-      const imgres = await axios.get(url, {
-        auth: { username: apiKey, password: apiSecret },
-      });
-      const data = imgres.data.result.colors;
+    const colorPallete = await getPaletteFromURL(imgurl, 5);
+    const data = colorPallete.map((c) => {
+      const hex = rgbToHex(c[0], c[1], c[2]);
+      return hex;
+    });
 
-      const colormind = await axios.post('http://colormind.io/api/', {
-        model: 'default',
-        input: [
-          [
-            data.background_colors[0].r,
-            data.background_colors[0].g,
-            data.background_colors[0].b,
-          ],
-          [
-            data.foreground_colors[0].r,
-            data.foreground_colors[0].g,
-            data.foreground_colors[0].b,
-          ],
-          [
-            data.image_colors[0].r,
-            data.image_colors[0].g,
-            data.image_colors[0].b,
-          ],
-          'N',
-          'N',
-        ],
-      });
+    const newImages = await imageService.addOneImage({
+      title: req.body.title,
+      imgurl: imgurl,
+      colorScheme1: `${data[0]}`,
+      colorScheme2: `${data[1]}`,
+      colorScheme3: `${data[2]}`,
+      colorScheme4: `${data[3]}`,
+      colorScheme5: `${data[4]}`,
+    });
 
-      if (colormind.status == 200) {
-        const newImages = await imageService.addOneImage({
-          title: req.body.title,
-          imgurl: imgurl,
-          colorScheme1: `rgb(${colormind.data.result[0]})`,
-          colorScheme2: `rgb(${colormind.data.result[1]})`,
-          colorScheme3: `rgb(${colormind.data.result[2]})`,
-          colorScheme4: `rgb(${colormind.data.result[3]})`,
-          colorScheme5: `rgb(${colormind.data.result[4]})`,
-        });
-
-        res.json(newImages);
-      }
-    } catch (error) {
-      res.send(Error(error.message));
-    }
+    res.json(newImages);
   })();
 });
 
--- utils.ts
+++ utils.ts
@@ -1,13 +1,13 @@
 const isNumber = (num: unknown): num is number => {
-  return typeof num === "number";
+  return typeof num === 'number';
 };
 const isString = (text: unknown): text is string => {
-  return typeof text === "string";
+  return typeof text === 'string';
 };
 
 const parseNumber = (num: unknown): number => {
   if (!num || !isNumber(num)) {
-    throw new Error("Incorrect or missing number input");
+    throw new Error('Incorrect or missing number input');
   }
 
   return num;
@@ -15,10 +15,19 @@ const parseNumber = (num: unknown): number => {
 
 const parseString = (comment: unknown): string => {
   if (!comment || !isString(comment)) {
-    throw new Error("Incorrect or missing string input");
+    throw new Error('Incorrect or missing string input');
   }
 
   return comment;
 };
 
-export { parseString, parseNumber };
+const componentToHex = (c: number) => {
+  const hex = c.toString(16);
+  return hex.length == 1 ? '0' + hex : hex;
+};
+
+const rgbToHex = (r: number, g: number, b: number) => {
+  return '#' + componentToHex(r) + componentToHex(g) + componentToHex(b);
+};
+
+export { parseString, parseNumber, rgbToHex };
